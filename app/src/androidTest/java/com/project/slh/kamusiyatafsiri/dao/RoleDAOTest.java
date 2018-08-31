eb5d3f0d594f9a7e0ddae9b524e343ec69f7fd21
asoilihi <lhabdou26@hotmail.fr>
2018-08-31 17:10:43 +0200

    aso - correction des tests unitaire

--- app/src/androidTest/java/com/project/slh/kamusiyatafsiri/dao/RoleDAOTest.java
+++ app/src/androidTest/java/com/project/slh/kamusiyatafsiri/dao/RoleDAOTest.java
@@ -3,6 +3,7 @@ package com.project.slh.kamusiyatafsiri.dao;
 import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
 import com.project.slh.kamusiyatafsiri.config.DAOBase;
 import com.project.slh.kamusiyatafsiri.entities.Role;
@@ -11,11 +12,13 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@RunWith(AndroidJUnit4.class)
 public class RoleDAOTest {
 
         private RoleDAO roleDAO;
